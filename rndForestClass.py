dba89e8a7304bd553926286e90919e261e289196
Cbouyssi <cesar.bouyssi@insa-lyon.fr>
2018-09-29 13:57:00 +0200

    Add test on nb of leafs and trees

--- rndForestClass.py
+++ rndForestClass.py
@@ -26,12 +26,50 @@ test = test.values.reshape(-1,28*28)
 
 classes = (0,1,2,3,4,5,6,7,8,9)
 
+def testMaxFeatures(X_train, Y_train, estimators):
+    with open('RndForestMaxFeaturesResults.txt','a') as f:
+        for i in [1,10,50,80,100,120,150]:
+            print(i)
+            clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = estimators, max_features = i)
+            scores = cross_val_score(clf, X_train, Y_train, cv=5)
+            np.savetxt(f, scores, delimiter = ".")
+            f.write('\n')
+            for iter in scores:
+                plt.plot(i, iter, 'bo')
+            plt.plot(i, np.mean(scores), 'ro')
+        clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = 10)
+        scores = cross_val_score(clf, X_train, Y_train, cv=5)
+        f.write('\n')
+        for iter in scores:
+            plt.plot(0, iter, 'go')
+        plt.plot(0, np.mean(scores), 'yo')
+        plt.show()
+
+def testMinLeaf(X_train, Y_train, maxFeatures, estimators):
+    with open('RndForestMaxMinLeaf.txt','a') as f:
+        for i in [1,10,50,80,100]:
+            print(i)
+            clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = estimators, max_features = maxFeatures, min_samples_leaf = i)
+            scores = cross_val_score(clf, X_train, Y_train, cv=5)
+            np.savetxt(f, scores, delimiter = ".")
+            f.write('\n')
+            for iter in scores:
+                plt.plot(i, iter, 'bo')
+            plt.plot(i, np.mean(scores), 'ro')
+        clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = 300, max_features = 50)
+        scores = cross_val_score(clf, X_train, Y_train, cv=5)
+        f.write('\n')
+        for iter in scores:
+            plt.plot(0, iter, 'go')
+        plt.plot(0, np.mean(scores), 'yo')
+        plt.show()
+
 #Function performing k-fold of the random forest methods on MNIST for several numbers of estimators
 def crossValidation(X_train, Y_train):
     fig = plt.figure()
 
     with open('RndForestResults.txt','a') as f:
-        for i in [1,10,20,30,40,50,60,80,100,200,500]:
+        for i in [70,75,80,85,90,95,100]:
             clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = i)
             scores = cross_val_score(clf, X_train, Y_train, cv=5)
             np.savetxt(f, scores, delimiter = ".")
@@ -58,8 +96,8 @@ def randomForestComfusion(X_train, Y_train, estimators):
 
 
 #Function generating the output file for kaggle submission. It receives the nb of estimators as a parameter
-def mnistRandomForest(X_train, Y_train, estimators):
-    clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = estimators)
+def mnistRandomForest(X_train, Y_train, estimators, maxFeatures):
+    clf = RandomForestClassifier(n_jobs=2, random_state=0, n_estimators = estimators, max_features = maxFeatures)
     clf.fit(X_train, Y_train)
     results = clf.predict(test)
 
@@ -68,6 +106,8 @@ def mnistRandomForest(X_train, Y_train, estimators):
     submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
     submission.to_csv("cnn_mnist_datagen.csv",index=False)
 
+#testMaxFeatures(X_train, Y_train, 300)
+#testMinLeaf(X_train, Y_train, 300, 50)
 #crossValidation(X_train, Y_train)
 #randomForestComfusion(X_train, Y_train, 10)
-#mnistRandomForest(X_train, Y_train, 10)
+mnistRandomForest(X_train, Y_train, 1000, 50)
